e910033cd542d6eebc2882aeea2917e3ed9db529
Patryk Ziemniak <ziemniak.pat@gmail.com>
2021-10-25 23:02:05 +0200

    Initial commit

---
+++ src/main/java/com/pzjd/travel/ecommerce/TravelEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.pzjd.travel.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelEcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelEcommerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pzjd/travel/ecommerce/TravelEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pzjd.travel.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelEcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
